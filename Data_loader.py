ea220a5a11b991a3cdadd6e888a25118a0a9a112
Victoria Firsanova <70071046+vifirsanova@users.noreply.github.com>
2021-02-07 12:20:00 +0300

    Update Data_loader.py

--- Data_loader.py
+++ Data_loader.py
@@ -1,4 +1,4 @@
-# VK API is used to load the data
+# VK API is used to load the data (для работы использовался VK API)
 # URL: https://vk.com/dev/methods
 
 %pip install vk_api
@@ -6,21 +6,20 @@ import json
 import vk_api
 def main():
 
-  # authorization 
+  # authorization (авторизация)
 
   vk_session = vk_api.VkApi('login', 'password')
   vk_session.auth()
 
-  # initializing VK tools
+  # initializing VK tools (инициализация VK tools)
 
   tools = vk_api.VkTools(vk_session)
 
-  # initializing wall.get method 
-  # wall.getComments or other methods can also be here
+  # initializing wall.get method (инициализация метода wall.get)
 
   wall = tools.get_all('wall.get', 100, {'domain': '---'})
 
-  # saving the JSON object with the data
+  # saving the JSON object with the data (сохранение JSON-объекта с полученными данными)
 
   with open('output.json', 'w') as fw:json.dump(wall, fw, ensure_ascii=False)
 
